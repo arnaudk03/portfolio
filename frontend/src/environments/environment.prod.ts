0b71c59303ca0b5a525839708043260f71154cea
arnaudk03 <wendgoudi69@gmail.com>
2026-08-10 14:16:05 +0000

    chore: update production API URL to Render endpoint

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://portfolio-backend-production.up.railway.app/api', // Remplacez par votre URL backend Render/Railway (ou '/api' si reverse proxy)
+  //apiUrl: 'https://portfolio-backend-production.up.railway.app/api', // Remplacez par votre URL backend Render/Railway (ou '/api' si reverse proxy)
+  apiUrl: 'https://portfolio-backend-e36r.onrender.com/api',
   githubUsername: 'arnaudk03',
   gitlabUsername: 'dcsi-dgtd',
   portfolioUrl: 'https://arnaudk03.github.io/portfolio',
